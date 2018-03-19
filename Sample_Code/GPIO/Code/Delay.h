a8b03181608c6820b3e88c2adf9af43cae6b42a8
nanming145k <1714013109@qq.com>
2018-03-19 11:09:36 -0700

    delt
    123

--- Sample_Code/GPIO/Code/Delay.h
+++
@@ -1,6 +0,0 @@
-void Delay10us(UINT16 u16CNT);
-void Delay_ms (UINT32 u32CNT);
-//void Timer1_Delay1ms (UINT32 u32CNT);
-//void Timer2_Delay1ms (UINT32 u32CNT);
-//void Timer3_Delay1ms (UINT32 u32CNT);
-//void WakeUp_Timer_Delay25ms(UINT32 u32CNT);
--- Sample_Code/GPIO/Code/selection.h
+++
@@ -1,6 +0,0 @@
-#ifndef __SELECTION_H
-#define __SELECTION_H
-
-void TX_Phase_ProcessSelect(void);
-
-#endif
